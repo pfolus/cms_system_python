5b5c28d38ae67a2e34c59152af58fdcf476bbadc
pfolus <imjanczalover18@gmail.com>
2017-07-04 15:27:31 +0200

    add codecoolers_list controller and codecoolers_list Class

--- controllers/codecoolers_list_controller.py
+++ controllers/codecoolers_list_controller.py
@@ -1,7 +1,20 @@
 import os.path
 import csv 
+from models.student_model import Student
+from models.mentor_model import Mentor
+from models.manager_model import Manager
+from models.accountant_model import Accountant
+from model.codecoolers_list import CodecoolersList
+
+
+def start_controller():
+    cc_list_object = CodecoolersList()
+    codecoolers_records = read_codecoolers_list_csv()
+    create_codecoolers(codecoolers_records)
+
 
 def read_codecoolers_list_csv():
+
     with open (os.path.dirname(__file__) + '../csv_databases/codecoolers_list.csv', 'r') as file:
         reader = csv.reader(file, delimiter='|')
         
@@ -10,6 +23,34 @@ def read_codecoolers_list_csv():
         for line in reader:
             codecoolers_list.append(line)
 
-    return (codecoolers_list)
-    
-read_codecoolers_list_csv()
+    return codecoolers_list
+
+
+def create_codecoolers(cc_list_object, codecoolers_records):
+
+    LOGIN_INDEX = 0
+    PASSWORD_INDEX = 1
+    NAME_INDEX = 2
+    SURNAME_INDEX = 3
+    TYPE_INDEX = 4
+
+    OBEJECT_STRING = 'user[TYPE_INDEX](user[LOGIN_INDEX], user[PASSWORD_INDEX], user[NAME_INDEX], user[SURNAME_INDEX])'
+
+    for user in read_codecoolers_records:
+        add_user_to_list(cc_list_obbject, eval(OBEJECT_STRING))
+
+
+def add_user_to_list(cc_list_object, user):
+
+    class_name = user.__class__.__name__
+
+    if class_name == "Student":
+        cc_list_object.students_list.append(user)
+    elif class_name == "Mentor":
+        cc_list_object.mentors_list.append(user)
+    elif class_name == "Accountant":
+        cc_list_object.accountants_list.append(user)
+    elif class_name == "Manager":
+        cc_list_object.managers_list.append(user)
+
+
