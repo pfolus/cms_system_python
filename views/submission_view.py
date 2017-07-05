4ffd5489c5576c834a17a80ebe9e979e738fe02d
domakdomak <domagalski.mt@gmail.com>
2017-07-05 21:33:09 +0200

    Import and use Bcolors class in submission_view

--- views/submission_view.py
+++ views/submission_view.py
@@ -1,3 +1,6 @@
+from views.bcolors import Bcolors
+
+
 def show_sub(submission):
     login = submission.user_login
     title = submission.title
@@ -5,8 +8,10 @@ def show_sub(submission):
     date = submission.date
     score = submission.score
     if submission.is_checked:
-        print('\033[92m'+"{}'s submission for assingment: {}: <{}> sent:{}, Score:{}".format(
-            login, title, answer, date, score)+'\033[0m')
+        print(Bcolors.GREEN + "\n{}'s submission for assingment: {}:\n <{}>\n sent:{}\n Score:{}".format(
+            login, title, answer, date, score) + Bcolors.ENDC)
+        print(Bcolors.BLUE + 'It was already checked by the mentor.' + Bcolors.ENDC)
     else:
-        print('\033[91m'+"{}'s submission for assingment: {}: <{}> sent:{}, Score:{}".format(
-            login, title, answer, date, score)+'\033[0m')
+        print(Bcolors.RED + "\n{}'s submission for assingment: {}:\n <{}>\n sent:{}\n Score:{}".format(
+            login, title, answer, date, score) + Bcolors.ENDC)
+        print(Bcolors.BLUE + 'It wasn\'t checked by the mentor yet.' + Bcolors.ENDC)
