6d6970f3e7c8b1b3f85e6ab657de55ce62b9fff5
domakdomak <domagalski.mt@gmail.com>
2017-07-05 11:12:36 +0200

    fix show_students_list in employee_view

--- views/employee_view.py
+++ views/employee_view.py
@@ -1,3 +1,10 @@
 def show_students_list(students_list):
+    index = 1
     for student in students_list:
-        print student
+        print('{}. {} {}'.format(index, student.name, student.surname))
+        index += 1
+
+
+def show_students_list_detailed(canvas):
+    # TODO
+    pass
