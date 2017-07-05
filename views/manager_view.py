70b7ed9ab2390d9a5e99ad719513b5b4a76f8ac0
Kamil Postrożny <kamilp009@gmail.com>
2017-07-05 12:05:57 +0200

    Add show mentors, option_error, print_menu and greet

---
+++ views/manager_view.py
@@ -0,0 +1,32 @@
+from employee_view imoprt *
+
+
+def greet(user):
+
+    print("Hello " + user.name + "!")
+
+
+def print_menu():
+
+    print('''1. List mentors
+          2. List mentors with details
+          3. Add mentor
+          4. Remove mentor
+          5. List students
+          6. List students with details
+          0. Log out
+          ''')
+
+
+def option_error():
+
+    print("There isn't such option!")
+
+
+def show_mentors(mentors):
+
+    index = 1
+
+    for mentor in mentors:
+        print("{}. {} {}".format(index, mentor.name, mentor.surname))
+        index += 1
