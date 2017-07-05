25a3fffd40810ed932f48e810b69b064d4f489e5
domakdomak <domagalski.mt@gmail.com>
2017-07-05 11:19:40 +0200

    Add Sumbission class in submission_model

---
+++ models/submission_model.py
@@ -0,0 +1,9 @@
+class Submission:
+
+    def __init__(self, user_login, title, answer, date, score=0, is_checked=False):
+        self.user_login = user_login
+        self.title = title
+        self.answer = answer
+        self.date = date
+        self.score = score
+        self.is_checked = is_checked
