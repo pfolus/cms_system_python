96ec62acbc195be252b19f2118a3c514b523b47a
domakdomak <domagalski.mt@gmail.com>
2017-07-05 16:45:49 +0200

    Add choose_assingment in student_controller

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -18,10 +18,24 @@ def run_chosen_function(user_input, canvas, user):
     if user_input == 1:
         show_grades(canvas.grades)
     elif user_input == 2:
-        show_assingments(canvas.assingments)
+        number = show_assingments(canvas.assingments)
+        chosen_assingment = choose_assingment(number, canvas.assingments)
     elif user_input == 0:
         return user_input
 
 
 def show_grades(grades):
     pass
+
+
+def choose_assingment(number, assingments):
+    choice = ''
+    possible_choices = range(0, len)
+
+    while choice not in possible_choices:
+
+        try:
+            choice = int(input("Choose assingment: "))
+        except ValueError:
+            print('Wrong number.')
+    return assingments[choice]
