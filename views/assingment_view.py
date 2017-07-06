5608b3c010707baa85010c158c2ed8108a6c24fc
domakdomak <domagalski.mt@gmail.com>
2017-07-06 13:38:45 +0200

    Fix show_assingment in assingment_view

--- views/assingment_view.py
+++ views/assingment_view.py
@@ -4,5 +4,5 @@ def show_assingment(assingment, number):
     date = assingment.date
     max_grade = assingment.max_grade
 
-    print('{}. {}:\n {}\nDeadline: {}\nScoring: 0-{}'.format(
+    print('\n{}. {}:\n {}\nDeadline: {}\nScoring: 0-{}'.format(
         number, title, content, date, max_grade))
