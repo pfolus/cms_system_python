032e99a240aebb3d11cbb0e52150c73d9fb296f5
Kamil Postrożny <kamilp009@gmail.com>
2017-07-06 14:05:48 +0200

    Fix show_students_list_detailed

--- views/employee_view.py
+++ views/employee_view.py
@@ -20,13 +20,10 @@ def show_students_list_detailed(canvas):
                                               canvas.submissions,
                                               student.login)
 
-        for attendace in canvas.attendances:
-            if attendace.student_login == student.login:
+        for attendance in canvas.attendances:
+            if attendance.student_login == student.login:
                 student_attendance = attendance.average
 
-        print('{}. {} {}, Score: {}/{}, Attendance: {:.f}%'.format(index, student.name,
-                                                                   student.surname,
-                                                                   grades_sum, max_grades_sum,
-                                                                   student_attendance))
+        print('{}. {} {}, Score: {}/{}, Attendance: {}%'.format(index, student.name, student.surname, grades_sum, max_grades_sum, student_attendance))
 
         index += 1
