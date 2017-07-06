21236689c274644660746c528ac69445581ba75d
Kamil Postrożny <kamilp009@gmail.com>
2017-07-06 10:59:26 +0200

    Add detailed students listing

--- views/employee_view.py
+++ views/employee_view.py
@@ -9,5 +9,21 @@ def show_students_list(students_list):
 
 
 def show_students_list_detailed(canvas):
-    # TODO
-    pass
+    index = 1
+    for student in canvas.students:
+        [grades_sum,
+         max_grades_sum,
+         amount_of_grades] = calculate_grades(canvas.assingments,
+                                              canvas.submissions,
+                                              student.login)
+
+        for attendace in canvas.attendances:
+            if attendace.student_login == student.login:
+                student_attendance = attendance.average
+
+        print('{}. {} {}, Score: {}/{}, Attendance: {:.f}%'.format(index, student.name,
+                                                                   student.surname,
+                                                                   grades_sum, max_grades_sum,
+                                                                   student_attendance))
+
+        index += 1
