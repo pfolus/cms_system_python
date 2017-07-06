e1d581e35b8bdd4a273acaac25b9b9d0f238b5ef
domakdomak <domagalski.mt@gmail.com>
2017-07-06 15:40:35 +0200

    Fix printing in show_assingment in assingment_view

--- views/assingment_view.py
+++ views/assingment_view.py
@@ -1,8 +1,11 @@
+from views.bcolors import Bcolors
+
+
 def show_assingment(assingment, number):
     title = assingment.title
     content = assingment.content
     date = assingment.date
     max_grade = assingment.max_grade
 
-    print('\n{}. {}:\n {}\nDeadline: {}\nScoring: 0-{}'.format(
-        number, title, content, date, max_grade))
+    print(Bcolors.BOLD + '{}. {}:\n{}\nDeadline: {}\nScoring: 0-{}\n'.format(
+        number, title, content, date, max_grade) + Bcolors.ENDC)
