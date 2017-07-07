40dca59aede4b3e89e7a2468036377be8ee93b6d
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:16:38 +0200

    Add docstrings to manager_model

--- models/manager_model.py
+++ models/manager_model.py
@@ -2,6 +2,12 @@ from models.codecooler_model import Codecooler
 
 
 class Manager(Codecooler):
+    '''
+    Initializes Manager instance
+
+    Args:
+        strings
+    '''
 
     def __init__(self, login, password, name, surname):
         super().__init__(login, password, name, surname)
