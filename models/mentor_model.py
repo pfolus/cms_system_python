5adb162eac19e14c1c324631b4af2880cdbce969
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:52:11 +0200

    Add docstring in mentor_model

--- models/mentor_model.py
+++ models/mentor_model.py
@@ -4,4 +4,13 @@ from models.codecooler_model import Codecooler
 class Mentor(Employee):
 
     def __init__(self, login, password, name, surname):
+        '''
+        Initializes Mentor instance
+
+        Args:
+            login - string
+            password - string
+            name - string
+            surname - string
+        '''
         super().__init__(login, password, name, surname)
