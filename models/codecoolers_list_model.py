4ee51640b993243142cd174834f8fd96bb702e3c
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:30:17 +0200

    Remove unnecessary file codecoolers_list_model

--- models/codecoolers_list_model.py
+++
@@ -1,8 +0,0 @@
-class CodecoolersList:
-
-    def __init__(self):
-        self.students_list = []
-        self.mentors_list = []
-        self.accountant_list = []
-        self.manager_list = []
-
