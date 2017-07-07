cf34d2b266ea350f99bba2a82e0b1ad2408c07d7
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:51:54 +0200

    Add docstrings in student_model

--- models/student_model.py
+++ models/student_model.py
@@ -4,4 +4,13 @@ from models.codecooler_model import Codecooler
 class Student(Codecooler):
 
     def __init__(self, login, password, name, surname):
+        '''
+        Initializes Student instance
+
+        Args:
+            login - string
+            password - string
+            name - string
+            surname - string
+        '''
         super().__init__(login, password, name, surname)
