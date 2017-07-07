4c12a06617965536ece4ddcdbdc97403934b6f03
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:51:27 +0200

    Add docstrings accountant_model

--- models/accountant_model.py
+++ models/accountant_model.py
@@ -2,5 +2,14 @@ from models.employee_model import Employee
 
 
 class Accountant(Employee):
+    '''
+    Initializes Accountant instance
+
+    Args:
+        login - string
+        password - string
+        name - string
+        surname - string
+    '''
     def __init__(self, login, password, name, surname):
         super().__init__(login, password, name, surname)
