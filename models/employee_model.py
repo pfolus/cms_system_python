88f18e4fc483265bd45b9d9013526a727df78383
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:43:37 +0200

    Add docstring in employee_model

--- models/employee_model.py
+++ models/employee_model.py
@@ -2,6 +2,15 @@ from models.codecooler_model import Codecooler
 
 
 class Employee(Codecooler):
+    '''
+    Initializes Employee instance
+
+    Args:
+        login - string
+        password - string
+        name - string
+        surname - string
+    '''
 
     def __init__(self, login, password, name, surname):
         super().__init__(login, password, name, surname)
