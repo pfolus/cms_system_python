9006f31d6c2b4f3d02f0d8f9b92750d220a7cef0
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:28:19 +0200

    Add docstrings in attendance_model

--- models/attendance_model.py
+++ models/attendance_model.py
@@ -1,6 +1,14 @@
 class Attendance:
 
     def __init__(self, student_login, student_attendances=[], average=float(0)):
+        '''
+        Initializes Attendance object
+
+        Args:
+            student_login - string
+            student_attendances - list with ints
+            average - float
+        '''
         self.student_login = student_login
         self.student_attendances = student_attendances
         self.average = average
