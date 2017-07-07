c821240711ed4fec7e706c8e8bd3fcb416a2dab3
Kamil Postrożny <kamilp009@gmail.com>
2017-07-07 09:52:03 +0200

    Add docstring in submission_model

--- models/submission_model.py
+++ models/submission_model.py
@@ -4,6 +4,17 @@ from datetime import datetime
 class Submission:
 
     def __init__(self, user_login, title, answer, date=datetime.now(), score=0, is_checked=False):
+        '''
+        Initializes Submission instance
+
+        Args:
+            user_login - string
+            title - string
+            answer - string
+            date - datetime object
+            score - int
+            is_checked - bool
+        '''
         self.user_login = user_login
         self.title = title
         self.answer = answer
