b1787228810b6796ae29188a65d9d93565109263
domakdomak <domagalski.mt@gmail.com>
2017-07-18 11:24:11 +0200

    Add add_to_list method in mentor_model

--- models/mentor_model.py
+++ models/mentor_model.py
@@ -1,7 +1,10 @@
 from models.employee_model import Employee
 from models.codecooler_model import Codecooler
 
+
 class Mentor(Employee):
+
+    mentors = []
 
     def __init__(self, login, password, name, surname):
         '''
@@ -14,3 +17,8 @@ class Mentor(Employee):
             surname - string
         '''
         super().__init__(login, password, name, surname)
+        Mentor.add_to_list(self)
+
+    @classmethod
+    def add_to_list(cls, object):
+        cls.mentors.append(object)
