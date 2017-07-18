9477c0457d2da7de82272c2abe35421fd684d2bb
Kamil Postrożny <kamilp009@gmail.com>
2017-07-18 10:54:08 +0200

    Fix attendance model

--- models/attendance_model.py
+++ models/attendance_model.py
@@ -1,14 +1,35 @@
+from datetime import datetime
+
+
 class Attendance:
 
-    def __init__(self, student_login, student_attendances=[], average=float(0)):
+    attendances = []
+
+    def __init__(self, student_login, value):
         '''
         Initializes Attendance object
 
         Args:
             student_login - string
-            student_attendances - list with ints
-            average - float
+            value - float
+            date - datetime object
         '''
         self.student_login = student_login
-        self.student_attendances = student_attendances
-        self.average = average
+        self.value = value
+        self.date = datetime.today()
+        Attendance.add_to_list(self)
+
+    @classmethod
+    def add_to_list(cls, object):
+        cls.attendances.append(object)
+
+    @classmethod
+    def get_student_attendances(cls, login):
+
+        student_attendances
+
+        for attendance in cls.attendances:
+            if attendance.student_login == login:
+                student_attendances.append(attendance)
+
+        return student_attendances
