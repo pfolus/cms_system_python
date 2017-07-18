b35d30c3e59c8482fe6cf17423c31ed0d0391835
domakdomak <domagalski.mt@gmail.com>
2017-07-18 11:23:57 +0200

    Add add_to_list method in submission_model

--- models/submission_model.py
+++ models/submission_model.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 class Submission:
 
+    submissions = []
+
     def __init__(self, user_login, title, answer, date=datetime.now(), score=0, is_checked=False):
         '''
         Initializes Submission instance
@@ -21,3 +23,8 @@ class Submission:
         self.date = date
         self.score = score
         self.is_checked = is_checked
+        Submission.add_to_list(self)
+
+    @classmethod
+    def add_to_list(cls, object):
+        cls.submissions.append(object)
