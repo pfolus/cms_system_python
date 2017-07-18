303d59e5611bae41ca5fbb5e82ac213b2b399a9f
domakdomak <domagalski.mt@gmail.com>
2017-07-18 11:24:03 +0200

    Add add_to_list method in student_model

--- models/student_model.py
+++ models/student_model.py
@@ -3,6 +3,8 @@ from models.codecooler_model import Codecooler
 
 class Student(Codecooler):
 
+    students = []
+
     def __init__(self, login, password, name, surname):
         '''
         Initializes Student instance
@@ -14,3 +16,8 @@ class Student(Codecooler):
             surname - string
         '''
         super().__init__(login, password, name, surname)
+        Student.add_to_list(self)
+
+    @classmethod
+    def add_to_list(cls, object):
+        cls.students.append(object)
